062fa59c165441fc95401b9557f612aceb3e44da
Pooja Mangal <91134877+Poojaamangal@users.noreply.github.com>
2024-05-07 15:13:29 +0000

    conversion of data type

---
+++ basics_js/03_conversionop.js
@@ -0,0 +1,20 @@
+let score="33"
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof  valueInNumber );
+
+//when we give a string that can not be convert in string then will give result  NaN
+//"33" = 33
+//"33abc" = NaN
+//null= 0
+//"pooja" = Nan
+//true = 1,false=0
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//"" - false
+//"pooja" - true
+
+let sm = 33
+let stringNumber = String(sm)
+console.log(typeof stringNumber)
